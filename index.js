7775b14980c251e2529400ff8848d3236dc4fb45
Petryck William <petryck@i9devgroup.com>
2023-03-11 19:43:48 -0300

    .
    .

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
-
-
-url = 'https://www.rstransitarios.com.br/';
-
 const autocannon = require('autocannon');
+const url = 'https://www.rstransitarios.com.br/';
 
-
-// Configuração para teste de carga
-const loadTest = autocannon({
-  url: url,
-  connections: 5000, // aumenta o número de conexões simultâneas
-  pipelining: 50, // aumenta o número de pipelines
-  duration: '30s', // aumenta o tempo de duração do teste
-  amount: 5000000, // aumenta o número de solicitações realizadas
-});
-
-// Evento para tratar as respostas das requisições
-loadTest.on('response', (client, statusCode, resBytes, responseTime) => {
-  console.log(`Status code: ${statusCode}`);
-  console.log(`Response time: ${responseTime} ms`);
-});
-
-// Evento para tratar o fim do teste de carga
-loadTest.on('done', (result) => {
-  console.log(result);
-});
+// Valida a URL de entrada
+if (!/^https?:\/\//i.test(url)) {
+  throw new Error('A URL deve começar com "http://" ou "https://"');
+}
 
 // Configuração para teste de segurança
 const securityTest = autocannon({
-  url: url,
-  connections: 500, // aumenta o número de conexões simultâneas
-  pipelining: 50, // aumenta o número de pipelines
-  duration: '1m', // aumenta o tempo de duração do teste
-  amount: 50000, // aumenta o número de solicitações realizadas
+  url,
+  connections: 5000,
+  pipelining: 50,
+  duration: '1m',
   headers: {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:58.0) Gecko/20100101 Firefox/58.0',
     'Referer': url,
     'Cookie': 'session_id=1234567890',
   },
+  // Configura um timeout para as requisições
+  timeout: 10000,
+  // Configura um limite de tamanho para as respostas
+  maxResponseSize: 1024 * 1024, // 1 MB
 });
 
 // Evento para tratar as respostas das requisições do teste de segurança
@@ -50,14 +34,16 @@ securityTest.on('done', (result) => {
   console.log(result);
 });
 
+// Evento para tratar erros na requisição
+securityTest.on('error', (err) => {
+  console.error(err);
+});
+
 // Inicia os testes de carga e segurança
-autocannon.track(loadTest, {renderProgressBar: true});
-autocannon.track(securityTest, {renderProgressBar: true});
+const runTests = async () => {
+  autocannon.track(securityTest, {renderProgressBar: true});
+  // Aguarda o término dos testes antes de encerrar o processo
+  await new Promise((resolve) => securityTest.once('done', resolve));
+};
 
-
-
-
-
-
-
-
+runTests();
